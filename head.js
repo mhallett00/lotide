b33daa3ee3c10fb60583382b9db354be847fd0df
Michael Hallett <m.hallett@gmail.com>
2020-04-27 22:38:26 +0000

    Adding Head function

---
+++ head.js
@@ -0,0 +1,27 @@
+//Assert alert function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Head function
+const head = function(input) {
+  if (input.length >= 1) {
+    
+    return input[0];
+  } else {
+    return undefined;
+  }
+
+};
+
+
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]),);
+
