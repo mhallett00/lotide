0b2b800386d2f8e849a07855a3a96ba2b40c6bb3
Michael Hallett <m.hallett@gmail.com>
2020-04-27 22:37:58 +0000

    Updating with delinted code

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,15 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
+  if (actual === expected) {
     console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-/*
+
 //TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 2);
-*/
