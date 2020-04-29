9517e6772df30d4c3fef3866b77762bc88cce79f
Michael Hallett <m.hallett@gmail.com>
2020-04-29 00:22:22 +0000

    renamed file from 'assertArrayEquals.js', refactored Assertion function

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,15 +1,12 @@
+// ARRAY ASSERTION FUNCTION
 const assertArrayEqual = function(numArray1, numArray2) {
-  let arrEqual;
   for (let i = 0; i < numArray1.length; i++) {
-    // console.log("current numbers:", numArray1[i], numArray2[i]);
-    if (numArray1[i] === numArray2[i]) {
-      arrEqual = true;
-    } else {
+    if (numArray1[i] !== numArray2[i]) {
       console.log(`❌️❌️❌️Assertion Failed: ${numArray1} !== ${numArray2}`);
       return;
     }
   }
-  console.log(`✔️ ✔️ ✔️ Assertion Passed: ${numArray1} === ${numArray2}`)
+  console.log(`✔️ ✔️ ✔️ Assertion Passed: ${numArray1} === ${numArray2}`);
 };
 
 assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
