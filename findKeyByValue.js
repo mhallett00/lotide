75b7d0e56bf8df9ed2700f58b6f6115c3b33d009
Michael Hallett <m.hallett@gmail.com>
2020-04-29 23:08:01 +0000

    Adding findKeyByValue.js to Lotide project. Accepts an object and searches within it's keys for a specified value.

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+// ARRAY ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION IMPLEMENTATION
+const findKeyByValue = function(genres, showChk) { 
+  for (const genre in genres) {
+    if (genres[genre] === showChk) {
+    return genre;
+    };
+  }
+  return undefined;
+};
+
+
+// TEST CODE
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 'sci_fi');
