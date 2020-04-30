b61a1248bf8e4e1f53915091ac1d0b0a52bfe82d
Michael Hallett <m.hallett@gmail.com>
2020-04-30 22:32:26 +0000

    Added assert test code.

--- map.js
+++ map.js
@@ -1,5 +1,24 @@
-const words = ["ground", "control", "to", "major", "tom"];
+// ARRAY ASSERTION FUNCTION
+const eqArrays = function(input1, input2) {
+  for (let i = 0; i < input1.length; i++) {
+    if (input1[i] !== input2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION IMPLEMENTATION
 const map = function(words, func) {
   const results = [];
   for (let item of words) {
@@ -8,5 +27,15 @@ const map = function(words, func) {
   return results;
 };
 
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = [];
+
 const results1 = map(words, word => word[0]);
-console.log(results1);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const results2= map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+const results3 = map (words2, []);
+assertArraysEqual(results3, []);
