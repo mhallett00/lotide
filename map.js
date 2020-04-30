a05905d6da409a5cb30cdbe63bd96b98584b34ac
Michael Hallett <m.hallett@gmail.com>
2020-04-30 21:34:43 +0000

    Adding map.js to Lotide project. map.js takes an in put array and return the first character of each array element.

---
+++ map.js
@@ -0,0 +1,12 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(words, func) {
+  const results = [];
+  for (let item of words) {
+    results.push(func(item))
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
