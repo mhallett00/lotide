678a2d235fe3bebf22bd1dbf2f3cb92a76dbe35b
Michael Hallett <m.hallett@gmail.com>
2020-05-01 01:08:09 +0000

    Adding findKey.js. function takes a nested object and searches for a nested object value. returns the object property's parent property.

---
+++ findKey.js
@@ -0,0 +1,64 @@
+// ASSERT TEST FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION IMPLEMENTATION
+const findKey = function(input, func) {
+  result = '';
+  for (let star in input) {
+    if (func(input[star])) {
+      return star;
+    }
+  }
+  return undefined;
+};
+
+//TEST CODE
+
+const constel = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+assertEqual(constel, "noma");
+
+const constel2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => 'Akaleri'
+assertEqual(constel2, "Akaleri");
+
+const constel3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4) // => undefined
+assertEqual(constel3, undefined);
+
+const constel4 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0) // => undefined
+assertEqual(constel4, undefined);
+
+
