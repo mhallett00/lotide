1332b8fb98f77219f5fdfbc0bb2a67fdb683be44
Michael Hallett <m.hallett@gmail.com>
2020-05-01 01:11:35 +0000

    Added extra testing condition.

--- map.js
+++ map.js
@@ -34,8 +34,8 @@ const words2 = [];
 const results1 = map(words, word => word[0]);
 assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
 
-const results2= map(words, word => word.length);
+const results2 = map(words, word => word.length);
 assertArraysEqual(results2, [6, 7, 2, 5, 3]);
 
-const results3 = map (words2, []);
+const results3 = map(words2, []);
 assertArraysEqual(results3, ['hi']); // should fail
