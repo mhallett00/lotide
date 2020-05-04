83df22a240a9b5945ebf2777c6eb1784c2b23f93
Michael Hallett <m.hallett@gmail.com>
2020-05-04 17:12:08 +0000

    MAde function exportable. Moved test code to middleTest.js.

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-// ARRAY ASSERTION FUNCTION
-const eqArrays = function(input1, input2) {
-  for (let i = 0; i <= input1.length; i++) {
-    if (input1[i] !== input2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const middle = function(array) {
   const middleIndex = Math.floor(array.length / 2);
   const outputArray = [];
@@ -30,6 +10,4 @@ const middle = function(array) {
   
 };
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+module.exports = middle;
