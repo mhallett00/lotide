1198b1704e150adaf6ac73faebeddcfafb0e6b4f
Michael Hallett <m.hallett@gmail.com>
2020-05-04 19:08:21 +0000

    Refactored tailTest.js to use mocha & chai assertion.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,29 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//TEST CODE
-//checking resulting array
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //checking resulting array is the required number of elements
-assertEqual(result[0], "Lighthouse"); //checking first element is now "Lighthouse"
-assertEqual(result[1], "Labs"); //checking second element is now "Labs"
+describe("#tail", () => {
+  const result = tail(["Hello", "Lighthouse", "Labs"]);
+  
+  it("returns an array of the right size", () => {
+    assert.strictEqual(result.length, 2);
+  });
 
-//checking initial array
-const words = ["Yo Yo", "Ligthouse", "Labs"];
-tail(words); //calling the function
-assertEqual(words.length, 3); //shows inital arrays still has 3 elements
+  it('returns ["Lighthouse", "Labs"] from ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
 
-//checking single element array returns empty tail
-const result2 = tail(["Hello"]);
-assertEqual(result2.length, 0); //shows resulting array has no elements
+  it('returns input array intact', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 
-//checking an empty array yields empty array
-const result3 = tail([]);
-assertEqual(result3.length, 0); //shows resulting array has no elements
+  it('returns empty tail for single element arrays', () => {
+    assert.strictEqual(tail(['Hello']).length, 0);
+  });
+
+  it('returns and empty array from an empty array', () => {
+    assert.strictEqual(tail([]).length, 0);
+  });
+
+});
