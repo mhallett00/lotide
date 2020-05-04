e70429c20f418ca43c2bf23073d4335b14fda80d
Michael Hallett <m.hallett@gmail.com>
2020-05-04 16:48:41 +0000

    Refactored head to push test code to headTest.js and require assertEqual.js

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-//Assert alert function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //Head function
-const head = function(input) {
+const head = (input) => {
+
   if (input.length >= 1) {
-    
     return input[0];
   } else {
     return undefined;
@@ -18,10 +9,4 @@ const head = function(input) {
 
 };
 
-
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]),);
-
+module.exports = head;
