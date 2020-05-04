ef68da1ffbdbe36a60dd7ca2fa8ebb310cd97267
Michael Hallett <m.hallett@gmail.com>
2020-05-04 17:06:53 +0000

    Made function exportable. Moved test code to test assertArraysEqualTest.js.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-// ARRAY ASSERTION FUNCTION
-const eqArrays = function(input1, input2) {
-  for (let i = 0; i <= input1.length; i++) {
-    if (input1[i] !== input2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -17,12 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertArraysEqual(["1"], []);
-assertArraysEqual([], ["1"]);
-assertArraysEqual([], []);
+module.exports = assertArraysEqual;
