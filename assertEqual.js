00227892bf035093a8cb8efd902cb50f1ace7550
Michael Hallett <m.hallett@gmail.com>
2020-05-04 16:47:10 +0000

    Refactored assertEqual to be exportable and to push test code to assertEqualTest.js

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-//TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 2);
+module.exports = assertEqual;
