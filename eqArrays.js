9e160ec2e41c81981d768f5ee8dd09e2b8f4150f
Michael Hallett <m.hallett@gmail.com>
2020-05-04 17:07:26 +0000

    Made function exportable. Moved test code to eqArraysTest.js.

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// ASSERTION TEST
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(input1, input2) {
   for (let i = 0; i <= input1.length; i++) {
     if (input1[i] !== input2[i]) {
@@ -16,12 +7,4 @@ const eqArrays = function(input1, input2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), false); // => should PASS
+module.exports = eqArrays;
