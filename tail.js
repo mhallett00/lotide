3fb49bfb89fa93017311d1feb00ff17c56ab6142
Michael Hallett <m.hallett@gmail.com>
2020-05-04 16:56:24 +0000

    made tail.js exportable and pushed test code to tailTest.js.

--- tail.js
+++ tail.js
@@ -1,35 +1,7 @@
-//ASSERT ALERT FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //TAIL FUNCTION
 const tail = function(array) {
   return array.slice(1);
 
 };
 
-
-//TEST CODE
-//checking resulting array
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //checking resulting array is the required number of elements
-assertEqual(result[0], "Lighthouse"); //checking first element is now "Lighthouse"
-assertEqual(result[1], "Labs"); //checking second element is now "Labs"
-
-//checking initial array
-const words = ["Yo Yo", "Ligthouse", "Labs"];
-tail(words); //calling the function
-assertEqual(words.length, 3); //shows inital arrays still has 3 elements
-
-//checking single element array returns empty tail
-const result2 = tail(["Hello"]);
-assertEqual(result2.length, 0); //shows resulting array has no elements
-
-//checking an empty array yields empty array
-const result3 = tail([]);
-assertEqual(result3.length, 0); //shows resulting array has no elements
+module.exports = tail;
