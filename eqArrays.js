85b81eaaf873b06858e003036909544f56c92d0a
Michael Hallett <63163884+mhallett00@users.noreply.github.com>
2020-08-20 11:21:08 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,13 @@
-const eqArrays = function(input1, input2) {
-  for (let i = 0; i <= input1.length; i++) {
-    if (input1[i] !== input2[i]) {
-      return false;
+const eqArrays = function (input1, input2) {
+  if (input1.length === input2.length) {
+    for (let i = 0; i <= input1.length; i++) {
+      if (input1[i] !== input2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
+  return false;
 };
 
 module.exports = eqArrays;
